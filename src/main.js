bfc01f9b1f46a2d4409eddba13792d637083e1a2
Dmitri <orldm@mail.ru>
2016-08-21 21:06:45 +0300

    add authors page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
+var Authors = require('./components/authors/authorPage');
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
 var App = React.createClass({
@@ -8,6 +9,8 @@ var App = React.createClass({
         var Child;
         switch(this.props.route) {
             case 'about': Child = About;
+            break;
+            case 'authors': Child = Authors;
             break;
             default: Child = Home;
         }
