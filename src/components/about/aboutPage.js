c58cfb313453447cb97d0ef649e12247b4d8cfe2
Dmitri <orldm@mail.ru>
2016-08-21 16:30:14 +0300

    add about page

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,23 @@
+"use strict";
+var React = require('react');
+var About = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This app uses following tchnologies:
+                    <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Node</li>
+                        <li>Gilp</li>
+                        <li>Browserify</li>
+                    </ul>
+                </p>
+            </div>
+        );
+    }
+});
+module.exports = Home;
