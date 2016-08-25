e9570bbdf242891a4beaec3f1c500c6712151ac0
Dmitri <orldm@mail.ru>
2016-08-25 09:01:30 +0300

    add transitions

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -1,6 +1,20 @@
 "use strict";
 var React = require('react');
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm('Are you sure?')) {
+                transition.abort();
+            } else {
+                callback();
+            }
+        },
+        willTransitionFrom: function(transition, component) {
+            if (!confirm('Are you sure?')) {
+                transition.about();
+            }
+        }
+    },
     render: function() {
         return (
             <div>
